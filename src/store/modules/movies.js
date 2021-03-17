26743347e84ca244d839358f478a136aac5478b2
MrMasimo <masimo91@mail.ru>
2021-03-17 21:57:48 +0300

    connect movieAPI

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,21 +1,54 @@
 import axios from '@/plugins/axios';
+import Ids from "../mock/imdb_top250";
+import mutations from '../mutations';
+
+function serializeResponse(movies)
+{
+  return movies.reduce((acc, movie) => {
+    acc[movie.imdbID] = movie;
+    return acc;
+  },{})
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    top250Ids: Ids,
+    currentPage: 1,
+    moviesPerPage: 12,
+    curMovies: {},
+  },
+  getters: {
+    getCurrentPage: ({ currentPage }) => currentPage,
+    getMoviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+    getCurrentIds: ({ top250Ids }) => (from, to) => top250Ids.slice(from, to),
+  },
+  mutations: {
+    [MOVIES](state,value) {
+      state.curMovies = value;
+    }
+  },
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const response = await axios.get("/",{
-        params: {
-        apikey: process.env.VUE_APP_API_KEY,
-        plot: "full",
-        i: "tt3896198",
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { getCurrentPage, getMoviesPerPage, getCurrentIds } = getters;
+        const from = getCurrentPage * getMoviesPerPage - getMoviesPerPage;
+        const to = getCurrentPage * getMoviesPerPage;
+        const curIds = getCurrentIds(from, to);
+
+        const request = curIds.map((id) => axios.get(`/?i=${id}`));
+
+        const response = await Promise.all(request);
+        const movies = serializeResponse(response);
+
+        commit("MOVIES", movies);
+      } catch (error) {
+        console.log(error);
       }
-      });
-      console.log(response);
+
+
     }
   },
 }
